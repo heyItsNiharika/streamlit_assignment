70fff7d0caa9d66b3c7db119ab195ba943a8e92d
heyItsNiharika <100463816+heyItsNiharika@users.noreply.github.com>
2023-04-13 13:06:44 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+# Find the largest among the 3 given numbers(value greater than the other two).
+# Submit the url in the following 
+
+import streamlit as st 
+import pandas as pd
+
+st.header()
+
+st.write("""
+
+# Find Largest Number 
+
+Given three numbers, this app finds the largest number among those three
+
+	""")
+
+st.header("Enter the numbers")
+
+def user_input():
+	num_1 = st.number_input("Enter First Number")
+	num_2 = st.number_input("Enter Second Number")
+	num_3 = st.number_input("Enter Third Number")
+	# , min_value = -2**30, max_value=2**30
+
+	data = {"First Number" : num_1,
+	"Second Number" : num_2,
+	"Third Number" : num_3}
+
+	features = pd.DataFrame(data, index=[0])
+	return features
+
+df = user_input()
+
+st.subheader("User Input Parameters")
+st.write(df.to_dict())
+
+st.table(df)
+
+
+num_1 = df['First Number'][0]
+num_2 = df['Second Number'][0]
+num_3 = df['Third Number'][0]
+
+ans = max(num_1, num_2, num_3)
+
+st.write("The maximum number is : ", ans)
+
+
+
